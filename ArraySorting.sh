021c8d493a47cc9f15615f0185525684b279edb5
nikita <nikitasah4gmail.com>
2022-08-06 01:24:35 +0530

    update

--- ArraySorting.sh
+++ ArraySorting.sh
@@ -29,7 +29,3 @@ secondSmallest=${arr[1]};
 echo "Second Smallest: $secondSmallest";
 arraySorting.sh
 Displaying arraySorting.sh.
-TFP-LT-567-Ashish-072022-Linux-2.4
-567-Ashish-July 22: 07:30 PM - 09:30 PM
-Upcoming
-Woohoo, no work due soon!
